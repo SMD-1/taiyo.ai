80a83bc415df8132f022bf017dcf09681b84514c
skdan <skdanish1999@gmail.com>
2023-05-10 20:35:18 +0530

    create contact, dashboard page and routing using react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import Sidebar from "./components/Sidebar.tsx";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.tsx";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+export const RootLayout = () => {
+  return (
+    <>
+      <Sidebar />
+      <Outlet />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <App /> },
+      { path: "/dashboard", element: <Dashboard /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
