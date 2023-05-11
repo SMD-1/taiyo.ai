14ecbf7153f133a584f81ed4bd877a7c5b10e26b
skdan <skdanish1999@gmail.com>
2023-05-11 12:32:49 +0530

    complete routing

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,20 @@ import "./index.css";
 import Sidebar from "./components/Sidebar.tsx";
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import Dashboard from "./pages/Dashboard.tsx";
+import { Provider } from "react-redux";
+import { contactReducer } from "./store/contactReducer.tsx";
+import AddContact from "./components/AddContact.tsx";
+import { createStore } from "redux";
+import EditContact from "./components/EditContact.tsx";
+
+const store = createStore(contactReducer);
 
 export const RootLayout = () => {
   return (
-    <>
+    <div className="md:flex md:h-screen">
       <Sidebar />
       <Outlet />
-    </>
+    </div>
   );
 };
 
@@ -22,12 +29,16 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <App /> },
       { path: "/dashboard", element: <Dashboard /> },
+      { path: "/add", element: <AddContact /> },
+      { path: "/edit/:id", element: <EditContact /> },
     ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
